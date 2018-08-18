d56957a28fc6a6dfe4da42858eb97cdd40d93d77
Doug Morrison <doug.morrison91@gmail.com>
2018-08-18 12:44:16 +1000

    Added script to set default parameters for the panda

---
+++ franka_control_wrappers/ros_nodes/set_panda_defaults.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import rospy
+from franka_control.srv import SetForceTorqueCollisionBehavior, SetForceTorqueCollisionBehaviorRequest
+from franka_control.srv import SetEEFrame, SetEEFrameRequest
+
+rospy.init_node('set_panda_defaults')
+
+rospy.wait_for_service('/franka_control/set_force_torque_collision_behavior')
+ftcb_srv = rospy.ServiceProxy('/franka_control/set_force_torque_collision_behavior', SetForceTorqueCollisionBehavior)
+ftcb_msg = SetForceTorqueCollisionBehaviorRequest()
+ftcb_msg.lower_torque_thresholds_nominal = [20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0]
+ftcb_msg.upper_torque_thresholds_nominal = [20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0]
+ftcb_msg.lower_force_thresholds_nominal = [5.0, 5.0, 5.0, 5.0, 5.0, 5.0]  # These will be used by the velocity controller to stop movement
+ftcb_msg.upper_force_thresholds_nominal = [20.0, 20.0, 20.0, 25.0, 25.0, 25.0]
+
+res = ftcb_srv.call(ftcb_msg).success
+if not res:
+    rospy.logerr('Failed to set Force/Torque Collision Behaviour Thresholds')
+else:
+    rospy.loginfo('Successfully set Force/Torque Collision Behaviour Thresholds')
+
+
+rospy.wait_for_service('/franka_control/set_EE_frame')
+eef_srv = rospy.ServiceProxy('/franka_control/set_EE_frame', SetEEFrame)
+eef_msg = SetEEFrameRequest()
+eef_msg.F_T_EE = [0.707099974155426, -0.707099974155426, 0.0, 0.0, 0.707099974155426, 0.707099974155426, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.035 + 0.13839999943971634, 1.0]
+
+res = eef_srv.call(eef_msg).success
+if not res:
+    rospy.logerr('Failed to set EE Frame')
+else:
+    rospy.loginfo('Successfully set EE Frame')
