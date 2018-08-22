911ace99bf6e53802298f7cdf9f5ad3e5017a20c
Doug Morrison <doug.morrison91@gmail.com>
2018-08-22 13:56:06 +1000

    Added surface normals and commented them all out.

--- franka_control_wrappers/src/franka_control_wrappers/panda_commander.py
+++ franka_control_wrappers/src/franka_control_wrappers/panda_commander.py
@@ -127,11 +127,14 @@ class PandaCommander(object):
         client.send_goal(franka_gripper.msg.HomingGoal())
         return client.wait_for_result()
 
-    def set_gripper(self, width, speed=0.1):
+    def set_gripper(self, width, speed=0.1, wait=True):
         client = actionlib.SimpleActionClient('franka_gripper/move', franka_gripper.msg.MoveAction)
         client.wait_for_server()
         client.send_goal(franka_gripper.msg.MoveGoal(width, speed))
-        return client.wait_for_result()
+        if wait:
+            return client.wait_for_result()
+        else:
+            return True
 
     def grasp(self, width=0, e_inner=0.1, e_outer=0.1, speed=0.1, force=15):
         client = actionlib.SimpleActionClient('franka_gripper/grasp', franka_gripper.msg.GraspAction)
--- mvp_grasping/ros_nodes/ap_grasping_velo.py
+++ mvp_grasping/ros_nodes/ap_grasping_velo.py
@@ -5,6 +5,8 @@ from __future__ import division, print_function
 import rospy
 import tf.transformations as tft
 
+import time
+
 import numpy as np
 
 from std_msgs.msg import Empty
@@ -33,6 +35,7 @@ class ActiveGraspController:
         self.curr_velo_pub = rospy.Publisher('/cartesian_velocity_node_controller/cartesian_velocity', Twist, queue_size=1)
         self.curr_velo = Twist()
         self.best_grasp = Pose()
+        # self.grasp_normal = np.array([0, 0, 1])
         self.grasp_width = 0.07
 
         self.update_rate = 10.0  # Hz
@@ -51,7 +54,7 @@ class ActiveGraspController:
 
         self.pregrasp_pose = [(rospy.get_param('/grasp_entropy_node/histogram/bounds/x2') + rospy.get_param('/grasp_entropy_node/histogram/bounds/x1'))/2,
                               (rospy.get_param('/grasp_entropy_node/histogram/bounds/y2') + rospy.get_param('/grasp_entropy_node/histogram/bounds/y1'))/2 + 0.08,
-                              0.55,
+                              0.60,
                               2**0.5/2, -2**0.5/2, 0, 0]
 
     def __robot_state_callback(self, msg):
@@ -76,8 +79,22 @@ class ActiveGraspController:
         self.curr_velo.linear.x = delta.x * vscale
         self.curr_velo.linear.y = delta.y * vscale
         self.curr_velo.linear.z = delta.z * vscale
+        # self.curr_velo.linear.z = -1 * max(0.01, 0.075 - (delta.x**2 + delta.y**2)**0.5)
 
         if len(res.best_grasp.data) > 0:
+            # max_angle = 10 / 180 * np.pi
+            # if res.best_grasp.data[7] < np.cos(max_angle):
+            #     s = np.array([res.best_grasp.data[5], res.best_grasp.data[6], np.cos(max_angle)])
+            #     s[0:2] = s[0:2]/(np.sqrt((s[0]**2 + s[1]**2)/(1-s[2]**2)))
+            #     print(np.linalg.norm(s))
+            #     # xyzw = [0, 0, 0, 1]
+            # else:
+            #     self.grasp_normal = np.array(res.best_grasp.data[5:])
+            # a = np.cross([0.0, 0.0, 1.0], self.grasp_normal)
+            # w = np.dot([0.0, 0.0, 1.0], self.grasp_normal)
+            # xyzw = np.array([a[0], a[1], a[2], w])
+            # xyzw /= np.linalg.norm(xyzw)
+
             # Publish pose.
             gp = Pose()
             gp.position.x = res.best_grasp.data[0]
@@ -85,11 +102,13 @@ class ActiveGraspController:
             gp.position.z = res.best_grasp.data[2]
             ang = res.best_grasp.data[3]
             q = tft.quaternion_from_euler(np.pi, 0, ang - np.pi/2)
+
+            # q = tft.quaternion_multiply(xyzw, q)
+
             gp.orientation.x = q[0]
             gp.orientation.y = q[1]
             gp.orientation.z = q[2]
             gp.orientation.w = q[3]
-            publish_pose_as_transform(gp, 'panda_link0', 'G', 0.05)
             self.best_grasp = gp
             self.grasp_width = res.best_grasp.data[4]
 
@@ -99,6 +118,7 @@ class ActiveGraspController:
             d_euler = tft.euler_from_quaternion(dq)
             self.curr_velo.angular.z = (d_euler[2])
 
+            publish_pose_as_transform(gp, 'panda_link0', 'G', 0.05)
         else:
             self.curr_velo.angular.z = 0
 
@@ -112,15 +132,16 @@ class ActiveGraspController:
             if self.ERROR_DETECTED:
                 return False
 
+            # End effector Z height
+            if self.robot_state.O_T_EE[-2] < 0.175: # - self.best_grasp.position.z < 0.15:
+                # self.stop()
+                rospy.sleep(0.1)
+                return True
+
             ctr += 1
             if ctr >= self.curr_velocity_publish_rate/self.update_rate:
                 ctr = 0
             self.__trigger_update()
-
-            # End effector Z height
-            if self.robot_state.O_T_EE[-2] < 0.15: # - self.best_grasp.position.z < 0.15:
-                # self.stop()
-                return True
 
             # Cartesian Contact
             if any(self.robot_state.cartesian_contact):
@@ -145,21 +166,35 @@ class ActiveGraspController:
             # Offset for initial pose.
             initial_offset = 0.1
             link_ee_offset = 0.138
+
+            # o = grasp_pose.orientation
+            # n = np.dot(tft.quaternion_matrix([o.x, o.y, o.z, o.w]), np.array([[0, 0, initial_offset + link_ee_offset, 0]]).T).flatten()
+            # angled_offset = n * -1
+
             grasp_pose.position.z = max(grasp_pose.position.z - 0.01, 0.025)  # 0.021 = collision with ground
             grasp_pose.position.z += initial_offset + link_ee_offset  # Offset from end efector position to
+            # grasp_pose.position.x += angled_offset[0]
+            # grasp_pose.position.y += angled_offset[1]
+            # grasp_pose.position.z += angled_offset[2]
 
+            self.pc.set_gripper(self.grasp_width, wait=False)
+            rospy.sleep(0.1)
             self.pc.goto_pose(grasp_pose, velocity=0.25)
 
             # Reset the position
             grasp_pose.position.z -= initial_offset + link_ee_offset
+            # grasp_pose.position.x -= angled_offset[0]
+            # grasp_pose.position.y -= angled_offset[1]
+            # grasp_pose.position.z -= angled_offset[2]
             #self.pc.goto_pose_cartesian(grasp_pose, velocity=0.1)
-
-            print(self.grasp_width)
-            self.pc.set_gripper(self.grasp_width)
 
             self.cs.switch_controller('velocity')
             v = Twist()
+            # angled_velo = -0.05 * angled_offset
             v.linear.z = -0.05
+            # v.linear.x = angled_velo[0]
+            # v.linear.y = angled_velo[1]
+            # v.linear.z = angled_velo[2]
             while self.robot_state.O_T_EE[-2] > grasp_pose.position.z and not any(self.robot_state.cartesian_contact) and not self.ERROR_DETECTED:
                 self.curr_velo_pub.publish(v)
                 rospy.sleep(0.01)
@@ -167,7 +202,7 @@ class ActiveGraspController:
             self.curr_velo_pub.publish(v)
 
             # close the fingers.
-            rospy.sleep(0.5)
+            rospy.sleep(0.2)
             self.pc.grasp(0, force=1)
 
             # Check for collisions
@@ -183,17 +218,26 @@ class ActiveGraspController:
         self.curr_velo_pub.publish(self.curr_velo)
 
     def go(self):
+        run_name = raw_input('Name of this run? ')
+        with open('/home/acrv/doug_logs/%s.txt' % run_name, 'w') as f:
+            f.write('Grasp\tSuccess\tTime\n')
+        i = 1
+        raw_input('Press Enter to Start.')
         while not rospy.is_shutdown():
             self.cs.switch_controller('moveit')
-            self.pc.goto_named_pose('grip_ready', velocity=0.5)
-            self.pc.goto_pose(self.pregrasp_pose, velocity=1.0)
+            self.pc.goto_named_pose('grip_ready', velocity=0.25)
+            start_pose = list(self.pregrasp_pose)
+            start_pose[0] += np.random.randn() * 0.05
+            start_pose[1] += np.random.randn() * 0.05
+            self.pc.goto_pose(start_pose, velocity=0.25)
             self.pc.set_gripper(0.1)
 
             self.cs.switch_controller('velocity')
-            raw_input('Press Enter to Start.')
+
             self.entropy_reset_srv.call()
             self.__trigger_update()
 
+            t0 = time.time()
             # Perform the velocity control portion.
             success = self.__velo_control_loop()
             if not success:
@@ -206,11 +250,17 @@ class ActiveGraspController:
                 continue
 
             gs = self.__execute_grasp(self.best_grasp)
+            t1 = time.time()
             if gs:
                 self.cs.switch_controller('moveit')
-                self.pc.goto_named_pose('grip_ready', velocity=0.5)
-                self.pc.goto_named_pose('drop_box', velocity=0.5)
+                self.pc.goto_named_pose('grip_ready', velocity=0.25)
+                self.pc.goto_named_pose('drop_box', velocity=0.25)
                 self.pc.set_gripper(0.07)
+
+            success = raw_input('Success? ')
+            with open('/home/acrv/doug_logs/%s.txt' % run_name, 'a') as f:
+                f.write('%d\t%s\t%f\n' % (i, success, t1-t0))
+            i += 1
 
 
 if __name__ == '__main__':
